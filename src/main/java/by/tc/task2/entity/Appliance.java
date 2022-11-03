d96403f80690bc237cc74c0b0ee7eeb6a86b0716
Vyacheslav <astapovich_work@gmail.com>
2022-11-03 18:26:17 +0300

    added appliance class

--- src/main/java/by/tc/task2/entity/Appliance.java
+++ src/main/java/by/tc/task2/entity/Appliance.java
@@ -3,5 +3,17 @@ package main.java.by.tc.task2.entity;
 
 
 public class Appliance {
-    
+    String name;
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Appliance:\n");
+        sb.append("\t").append(name).append(";\n");
+        return sb.toString();
+    }
+
+
+    public Appliance(String name){
+        this.name = name;
+    }
 }
